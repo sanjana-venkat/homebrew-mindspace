f6334da402d6ffcc77924dab402e6f896697acf6
abisubramanya27 <abisubramanya27@gmail.com>
2026-09-10 17:48:37 -0700

    mindspace 0.1.1

--- Casks/mindspace.rb
+++ Casks/mindspace.rb
@@ -1,6 +1,6 @@
 cask "mindspace" do
-  version "0.1.0"
-  sha256 "3bb62f28fc799a9fb7b1076187f04d85276e5247fb5585bd347aab19e9378327"
+  version "0.1.1"
+  sha256 "13fd2594362794dd5138ab5482cccc9ef8270d70019fea92688ba1f256381462"
 
   url "https://github.com/sanjana-venkat/mindspace/releases/download/v#{version}/Mindspace-#{version}.dmg"
   name "Mindspace"
@@ -10,15 +10,6 @@ cask "mindspace" do
   depends_on macos: ">= :sonoma"
 
   app "Mindspace.app"
-
-  # The build is ad-hoc signed rather than notarized, so Gatekeeper quarantines
-  # it on download. Drop the quarantine flag on install; remove this block once
-  # the app ships with a Developer ID signature and a notarization ticket.
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Mindspace.app"],
-                   sudo: false
-  end
 
   zap trash: [
     "~/Library/Application Support/Notefy",
